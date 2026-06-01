190d7dd8c7174a766e5aaadd28f01e7d94754809
moscow <malloc40k@gmail.com>
2026-06-01 08:50:43 +0000

    Add files via upload

--- init.lua
+++ init.lua
@@ -4,6 +4,18 @@ vim.g.mapleader = " "
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then 
+	local repo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system( {"git", "clone", "--filter=blob:none", "--branch=stable", repo, lazypath } )
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{"Failed: ", "ErrorMsg"},
+			{out},
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
 vim.opt.rtp:prepend(lazypath)
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
